9a8f7d690c8b344fe9dea2221dff792855797f90
Anthony Wratten <awratten1@gmail.com>
2017-08-28 08:53:03 +1000

    Update GetCurrentFinancialYear.py

--- GetCurrentFinancialYear.py
+++ GetCurrentFinancialYear.py
@@ -14,8 +14,8 @@ import time
 def GetCurrentFinancialYear():
     LastMonth = 6 #Last month of the financial year eg. June would be : 6
     CurrentYear = int(time.strftime("%Y"))
-    PreviousYear = int(time.strftime("%Y")) - 1
-    NextYear = int(time.strftime("%Y")) + 1
+    PreviousYear = CurrentYear - 1
+    NextYear = CurrentYear + 1
     FinYear = ""
     if (int(time.strftime("%m")) > LastMonth):
         FinYear = str(CurrentYear) + "/" + str(NextYear)
