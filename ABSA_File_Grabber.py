573bab83ddcd5f29061e341b76e8411c24777bca
Anthony Wratten <awratten1@gmail.com>
2017-08-28 08:55:16 +1000

    Update ABSA_File_Grabber.py

--- ABSA_File_Grabber.py
+++ ABSA_File_Grabber.py
@@ -2,18 +2,18 @@ import requests, requests.utils
 import re
 # import PyPDF2
 
+url = "https://www.absa.net.au/documents/item/"
 
 for num in range(999):
 	# num = 251
 	try:
-		r = requests.get("https://www.absa.net.au/documents/item/" + str(num))
+		r = requests.get(url + str(num))
 
 		d = r.headers['content-disposition']
 		fname = re.findall("filename=(.+)", d)
 
 		fname = fname[-1]
 		fname = fname.replace('"', '')
-		# fname_2 = ''.join(str(num), fname)
 
 		with open(str(num) + "_" + fname, 'wb') as f:
 			f.write(r.content)
